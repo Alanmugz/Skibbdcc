9abd6693dce998475ea6b5db1705f5688d1c38d9
Alanmugz <alanmugz@gmail.com>
2016-03-15 20:49:32 +0000

    F0000 - Updated global css

--- test.php
+++ test.php
@@ -180,8 +180,8 @@
 		<div class="row">
 			<div class="col-md-12">
 				<?php 
-						include('includebootstrap/copyright.html');
-					?>
+					include('includebootstrap/copyright.html');
+				?>
 			</div>
 		</div>
 	</div>
