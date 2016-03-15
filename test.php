bd8bd73344cb1faa8777e9b82ebcfc19ea49eb3b
amulligan <amulligan@continuumcommerce.com>
2016-03-15 11:05:02 +0000

    F0000 - Added common items to includes folder

--- test.php
+++ test.php
@@ -90,38 +90,17 @@
 				   <div class = "panel-heading">
 					  <h3 class = "panel-title">Club Sponsors</h3>
 				   </div>
-				   
-				   <div class = "panel-body">
-						<a href="http://www.westlodgehotel.ie/">Westlodge Hotel</a><br /> 
-						<a href="http://www.bantry.ie/">Bantry Development & Tourism Association</a><br /> 
-						<a href="http://www.carberyplastics.com/">Carbery Plastics</a><br />
-						<a href="omahony.html">O'Mahony Crash Repairs</a><br />
-						<a href="clonakiltypotatoes.html">Clonakilty Potatoes</a><br />
-						<a href="http://www.keohanereadymix.com/">Keohane Readymix</a><br />
-						<a href="http://www.plasticbags.ie/">Plastic Bags</a><br />
-						<a href="toureenconstruction.html">Toureen Construction</a><br />
-						<a href="blastit.html">Blast It</a><br />
-						<a href="johnfordeconstruction.html">John Forde Construction</a><br />
-						<a href="http://www.westcorkoil.ie/">Tria Oil</a><br />
-						<a href="hayescabins.html">Hayes Cabins</a><br />
-						<a href="http://www.blackwatermotors.ie/">Blackwater Motors</a><br />
-						<a href="lordantarmac.html">Lordan Tarmac</a><br />
-						<a href="mossiecabs.html">MossiesCabs</a><br />
-						<a href="n71odonovanmotorstheoldcreamery.html">N71 & O'Donovan Motors & The Old Creamery</a><br />
-						<a href="http://www.danseamanmotors.ie/">Dan Seaman Motors</a><br /> 
-						<a href="dandmcrashrepairs.html">D & M Crash Repairs</a>	
-				   </div>
+				   <?php 
+						include('includebootstrap/sponsors.html');
+					?>
 				</div>
 				<div class = "panel panel-default">
 				    <div class = "panel-heading">
 						<h3 class = "panel-title">Social Media</h3>
 				    </div>
-				   
-				    <div class = "panel-body">
-						<!-- https://twitter.com/settings/widgets/708238291645145088/edit?notice=WIDGET_UPDATED-->
-						<a class="twitter-timeline" href="https://twitter.com/Skibbdcc" data-widget-id="708238291645145088">Tweets by @Skibbdcc</a>
-						<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-				    </div>
+				   <?php 
+						include('includebootstrap/socialmedia.html');
+					?>
 				</div>
 			</div>
 		</div>
@@ -129,52 +108,39 @@
 			<div class="col-md-4">
 				<div class = "panel panel-default font">
 				    <div class = "panel-heading">
-					  <h3 class = "panel-title">Contact Us:</h3>
+						<h3 class = "panel-title">Contact Us:</h3>
 				    </div>
-				   
-				    <div class = "panel-body fill">
-						<a href="contact_us.php">Contact Skibbdcc</a><br />
-						<a href="contact_us.php">WebMaster</a><br />
-						<a href="contributors_login.php">Contributors Login</a>
-				    </div>
+				    <?php 
+						include('includebootstrap/contactus.html');
+					?>
 				</div>
 			</div>
 			<div class="col-md-4">
 				<div class = "panel panel-default font">
-				   <div class = "panel-heading">
+				    <div class = "panel-heading">
 						<h3 class = "panel-title">Club Events:</h3>
-				   </div>
-				   
-				   <div class = "panel-body fill">
-						<a href="fastnet_rally.php">Westlodge Hotel Fastnet Rally</a><br />
-						<a href="autotest.php">AutoTest</a><br />
-						<a href="loose_surface_autocross.php">Loose Surface AutoCross</a><br />
-						<a href="100_isles_night_nav.php">100 Isles Night Nav</a><br />
-						<a href="carbery_night_nav.php">Carbery Night Nav</a><br />
-						<a href="economy_run.php">Treasure Hunt</a><br />
-						<a href="club_championship.php">The <span id="getSponsor"></span> Club Championship</span></a>
-				   </div>
+				    </div>
+				    <?php 
+						include('includebootstrap/clubevents.html');
+					?>
 				</div>			
 			</div>
 			<div class="col-md-4">
 				<div class = "panel panel-default font">
-				   <div class = "panel-heading">
+				    <div class = "panel-heading">
 						<h3 class = "panel-title">In Association With:</h3>
-				   </div>
-				   
-				   <div class = "panel-body fill">
-					   <a href="http://www.nationalirishrallychampionship.com/" target="_blank">Triton Showers National Rally Championship</a><br />
-					   <a href="http://www.southern4rallychampionship.com/" target="_blank">Southern 4 Rally Championship</a><br />
-					   <a href="http://www.motorsportireland.com/" target="_blank">Motorsport Ireland</a></span>
-				   </div>
+				    </div>
+					<?php 
+						include('includebootstrap/association.html');
+					?>
 				</div>			
 			</div>
 		</div>
 		<div class="row">
 			<div class="col-md-12">
-				<div id="copyright">
-					&copy; Skibbereen &amp; District Car Club 2011 - <?php echo date("Y"); ?><br />Designed by Alan Mulligan Web Design
-				</div>
+				<?php 
+						include('includebootstrap/copyright.html');
+					?>
 			</div>
 		</div>
 	</div>
