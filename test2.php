b066d1cdf9af89f5b3ad2fd2f834fed4cde1dd3f
amulligan <amulligan@continuumcommerce.com>
2016-03-31 11:56:25 +0100

    Added 100 isles night nav pages

--- test2.php
+++ test2.php
@@ -70,7 +70,7 @@ include 'php/eventenumertion.php';
         include ('includebootstrap/mobilemenu.html');
         ?>
     </div>		
-    <div style="display:inline-block;margin:-25px 0px 15px 0px;>
+    <div style="display:inline-block;margin:-25px 0px 15px 0px;">
         <?php
         include ('includebootstrap/event/100islesmenu.html');
         ?>
