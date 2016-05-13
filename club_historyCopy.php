3b5e4b45305636a56501dc74f2aff9d55c84654e
amulligan <amulligan@continuumcommerce.com>
2016-05-13 12:09:01 +0100

    F0000 - Added club history page

--- club_historyCopy.php
+++ club_historyCopy.php
@@ -80,9 +80,63 @@
 			<div class="col-md-8">
 				<div class = "panel panel-default">
 					<div class = "panel-heading">
-						<h3 class = "panel-title">Carbery Plastics Club Championship 2016</h3>
+						<h3 class = "panel-title">Club History</h3>
 					</div>
-					<!--Club Championship news here-->
+						<p style="padding-left:15px;padding-right:15px;">Mr. Michael O'Driscoll, who lived in Skibbereen was keenly interested in car Rallies and motoring in 
+						general and early in 1961 he organized a Car Road Safety Rally in Skibbereen. A large Committee was 
+						formed to run this rally and it was a major success.<br /><br />
+
+						As a result of a very successful Road Safety Rally, which attracted an entry of 82 drivers, was held 
+						in Skibbereen on September 24th 1961. With the £11 profit from this rally it was decided, in December 
+						1961, to form the West Cork Motorcycle and Car club.<br /><br />
+
+						In its heyday there were over 200 Club members and major Scramblers and Grasstrack races were organized 
+						in Skibbereen and Dunmanway. Some of these events attracted crowds of several thousand people, and were 
+						the main Sporting events in West Cork at this time.<br /><br />
+
+						Several Veteran Car runs which attracted up to 100 entries from all over Ireland and Britain were also 
+						organized and the Club also assisted in the running of the Circuit of Ireland Rally.<br /><br />
+
+						The club ceased to be active in the early 1970's. On November 1977 a meeting was held in O'Brien's 
+						Lounge Skibbereen to reform the Motor Club. In the lapsed years, car competitions became more popular 
+						than bikes and the new club was called Skibbereen Motor Club. The Club was affiliated o the R.I.A.C. 
+						in 1981 under its present name, Skibbereen and District Car Club and had its first dates on the 
+						calendar in 1982.<br /><br />
+
+						The first event run by the newly formed club was a Test-Trial in the public car park behind Gerald 
+						O'Brien's Lounge and J.J. Fields of Main Street Skibbereen.<br /><br />
+
+						In March 1982 the first rally was run by the Club. The Rally headquarters was Bernard O'Brien's, 
+						Main Street Skibbereen and the Rally was called 'The Motovox Single Stage Rally'. It was run over 
+						the Classic Tragumna Stage and was won by Demi Fitzgerald driving his famous black Chevette HSR. 
+						October 1982 also saw the running of the first 'Fastnet Inn Hotel Schull Single Stage Rally' 
+						which was won by Frank O'Mahony in his pristine Ford Escort RS 1800. 1989 saw the Marine Hotel 
+						Fastnet Rally as the final round of the Anglo Irish Bank Corp. National Stages Rally Championship.<br /><br />
+
+						The club continues to the present day and has approximately 225 club members. The club runs a range of 
+						popular successful events throughout the sporting year such as two double autotests, two autocrosses, 
+						two night navigation trials, an economy run and the club’s highlight of the year on the October Bank 
+						Holiday Weekend – The Fastnet Rally.<br /><br />
+
+						Skibbereen & District Car Club has made contributions to charity from monies raised at these events, 
+						namely St Michael’s Home for the Elderly in Bandon, Marymount Hospice Cork, Cystic Fibrosis Unit 
+						Cork University Hospital, West Cork Rapid Response, R.N.L.I, and others. We have also run events 
+						for the children from the “Share a dream foundation”.<br /><br />
+
+
+						The 2011 Westlodge Fastnet Rally moved from its home for the past  few years of Skibbereen to its new home 
+						of Bantry. Clerk of the course for the event was again Ger Hayes. Ger and his hard working team can be found 
+						week after week around the country helping out at all types of motorsport events.<br /><br />
+
+						The Present officers of the Club are:<br />
+						President: Alan Clarke<br />
+						Chairman: Colm Feen<br />
+						Secretary: Ger Hayes<br />
+						Treasurer: John Coleman<br />
+						Registrar: James Kingston<br />
+						P.R.O Padraig McCarthy<br /><br />
+
+						Club telephone number is 087 0560316</p>
 				</div>
 				
 				<div class = "panel panel-default visible-lg backgroundColor">
