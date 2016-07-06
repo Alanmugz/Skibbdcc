2f0d0004a6c0e7ae64b8e202b201116ae3ebd68a
Alanmugz <alanmugz@gmail.com>
2016-07-06 21:34:40 +0100

    F0000 - Added fastnet rally web pages

--- fastnet_entry_list.php
+++ fastnet_entry_list.php
@@ -39,10 +39,6 @@ include 'php/eventenumertion.php';
 
     <!-- Timer -->
     <link href="http://www.jqueryscript.net/css/jquerysctipttop.css" rel="stylesheet" type="text/css">
-	
-	<!-- Maps -->
-	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBGULXcf_6aRjzb5yokrYUkj0OXT6dHpyI&callback=initMap" async defer></script>
-	
 
 </head>
 <!-- Bottom menu -->
@@ -81,106 +77,52 @@ include 'php/eventenumertion.php';
     <div style="display:inline-block;margin:-25px 0px 15px 0px;">
     </div>		
     <div class="row">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class = "panel panel-default backgroundColor font">
                 <div class = "panel-heading">
-                    <h3 class = "panel-title">Westlodge Fastnet Rally <?php echo $rallyEventYear; ?> - Maps</h3>
+                    <h3 class = "panel-title">Westlodge Fastnet Rally <?php echo $rallyEventYear; ?> - Entry List</h3>
                 </div>
-				  <script type="text/javascript">
-function initialize() {
-	var i;
-	var arrDestinations = [
-		{
-			lat: 51.672662, 
-			lon: -9.474592, 
-			title: "HQ & Parc Ferme", 
-			description: "HQ & Parc Ferme",   
-		},
-		{
-			lat: 51.660737,   
-			lon: -9.470724,
-			title: "Scrutiny", 
-			description: "Scrutiny, Barrys Of Bantry"
-		},
-		{
-			lat: 51.663439,  
-			lon: -9.476234, 
-			title: "Trailer Park", 
-			description: "Trailer Park,\nBantry Business Park"
-		},
-		{
-			lat: 51.681177,   
-			lon: -9.454851, 
-			title: "Service Area", 
-			description: "Service Area"
-		}
-	];
-	
-	var myOptions = {
-		zoom: 13,
-		center: new google.maps.LatLng(51.673270, -9.467500),
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
-	
-	var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
-	
-	var infowindow =  new google.maps.InfoWindow({
-		content: ''
-	});
-	
-	// loop over our array 
-	for (i = 0; i < arrDestinations.length; i++) {
-		// create a marker
-		var marker = new google.maps.Marker({
-			title: arrDestinations[i].title,
-			position: new google.maps.LatLng(arrDestinations[i].lat, arrDestinations[i].lon),   
-			map: map
-		});
-		
-		// add an event listener for this marker 
-		bindInfoWindow(marker, map, infowindow, "<p class='tooltip'>" + arrDestinations[i].description + "</p>");  
-	}
-}
-
-function bindInfoWindow(marker, map, infowindow, html) { 
-	google.maps.event.addListener(marker, 'mouseover', function() { 
-		infowindow.setContent(html); 
-		infowindow.open(map, marker); 
-	}); 
-} 
-
-google.maps.event.addDomListener(window, 'load', initialize);
-</script>
-				<div id="map-canvas" style="width:97%;height:400px; margin: 10px auto 10px auto"></div>	
-            </div>
-        </div>
-
-        <div class="col-md-4">
-            <div class = "panel panel-default backgroundColor">
-                <div class = "panel-heading">
-                    <h3 class = "panel-title">Next Event:</h3>
-                </div>
-                <div id="countdown-nextmeeting">
-                    <?php
-                    include('includebootstrap/countdowntimer.html');
-                    ?>
-                </div>
-            </div>
-            <div class = "panel panel-default font">
-                <div class = "panel-heading">
-                    <h3 class = "panel-title">Club Sponsors</h3>
-                </div>
-                <?php
-                include('includebootstrap/sponsors.html');
-                ?>
-            </div>
-            <div class = "panel panel-default">
-                <div class = "panel-heading">
-                    <h3 class = "panel-title">Social Media</h3>
-                </div>
-                <?php
-                include('includebootstrap/socialmedia.html');
-                ?>
+				<div class="container" style="padding-right:35px">
+				  <h2>Main Field</h2>            
+				  <table class="table table-striped">
+					<thead>
+					  <tr>
+						<th>#</th>
+						<th>Driver</th>
+						<th>Co-Driver</th>
+						<th>Address</th>
+						<th>Car</th>
+						<th>Class</th>
+					  </tr>
+					</thead>
+					<tbody>
+					  <tr>
+						<th>1</th>
+						<th>Driver</th>
+						<th>Co-Driver</th>
+						<th>Address</th>
+						<th>Car</th>
+						<th>Class</th>
+					  </tr>
+					  <tr>
+						<th>2</th>
+						<th>Driver</th>
+						<th>Co-Driver</th>
+						<th>Address</th>
+						<th>Car</th>
+						<th>Class</th>
+					  </tr>
+					  <tr>
+						<th>3</th>
+						<th>Driver</th>
+						<th>Co-Driver</th>
+						<th>Address</th>
+						<th>Car</th>
+						<th>Class</th>
+					  </tr>
+					</tbody>
+				  </table>
+				</div>
             </div>
         </div>
     </div>
