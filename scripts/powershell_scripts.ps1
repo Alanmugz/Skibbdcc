cc1de26773449ddaf5ffeefb3bf4ed4320bfb06e
Alanmugz <alanmugz@gmail.com>
2016-09-15 22:58:15 +0100

    F0000 - Added powershell scripts

--- scripts/powershell_scripts.ps1
+++ scripts/powershell_scripts.ps1
@@ -4,13 +4,34 @@
 # To run the function
 #	
 
-
 #http://stackoverflow.com/questions/3822745/rename-files-to-lowercase-in-powershell
 
 function toLowerCaseAllFilesInDirectory($directory) {
 	If ($directory -eq $Null) { Write-error "Pass in the path of the directory you would like to lowercase" }
     dir $directory -r | % { if ($_.Name -cne $_.Name.ToLower()) { ren $_.FullName $_.Name.ToLower() } }
 }
+
+
+function addNewYearFolderToDirectory($year) {
+	If ($year -eq $Null) { Write-error "Pass in the year of the directory you wish to add to the events file folders" }
+    
+	$directories = @(
+		"100_isles_night_nav",
+		"autocross",
+		"autotest_august",
+		"autotest_may",
+		"carbery_night_nav",
+		"club_championship",
+		"loose_surface_autocross_february",
+		"loose_surface_autocross_july")
+		
+	$directories | foreach {
+		New-Item -ItemType directory -Path "C:\xamppnew\htdocs\dev.skibbdcc.com\files\$_\$year"
+	}
+}
+
+
+
 
 
 
