6cdd5380acf0368aa028a096c3658d08be331a0f
Alan Mulligan <Alanmugz@gmail.com>
2017-10-20 21:26:44 +0100

    F0000 - Added 2017 fastnet rally entry list

--- fastnet_entry_list.php
+++ fastnet_entry_list.php
@@ -96,7 +96,7 @@ include 'php/eventenumertion.php';
 						0 => array(
 							'name' => 'Main Field',
 							'file' => 'files/fastnet_2017/entry_list_main_field.csv'
-						)
+						),
 						//0 => array(
 						//    'name' => 'Main Filed',
 						//    'file' => 'files/fastnet_2016/entry_list_main_field.csv'
@@ -105,10 +105,10 @@ include 'php/eventenumertion.php';
 						//    'name' => 'Historics',
 						//    'file' => 'files/fastnet_2016/entry_list_historics.csv'
 						//),
-						//2 => array(
-						//    'name' => 'Juniors',
-						//    'file' => 'files/fastnet_2016/entry_list_juniors.csv'
-						//)
+						2 => array(
+							'name' => 'Juniors',
+							'file' => 'files/fastnet_2017/entry_list_juniors.csv'
+						)
 					);
 
 					foreach ($classDetails as $row) {
