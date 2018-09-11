e4550bbbb1aab70c6167e1757f1e3274616cbc00
Alan Mulligan <Alanmugz@gmail.com>
2018-09-11 23:06:55 +0100

    F0000 - Updated event coc's and contact numbers

--- php/config.php
+++ php/config.php
@@ -28,18 +28,18 @@ $fileYear = '2018';
 
 //Event date details
 $fileYear = '2018';
-$IslesNightNavigationTrialDetails = "100 Isle Night Navigation Trial 27th - 28th January 2018";
+$IslesNightNavigationTrialDetails = "100 Isle Night Navigation Trial February 2019";
 $IslesNightNavigationCOC = "C.O.C: Denis O Donovan 086 8773817";
-$LooseSurfaceAutocrossFebruaryDetails = "Loose Surface Autocross 25th February 2018";
-$LooseSurfaceAutocrossFebruaryCOC = "C.O.C: James Kingston 0868598038";
-$AutotestMayDetails = "Autotest 20th & 21st May 2017";
-$AutotestMayCOC = "C.O.C: Alan Clarke/DonGiles 0851744918/0868060604";
-$LooseSurfaceAutocrossJulyDetails = "Loose Surface Autocross 8th July 2018";
-$LooseSurfaceAutocrossJulyCOC = "C.O.C: Leslie Wolfe 0860666688";
-$AutotestAugustDetails = "Autotest 19th & 20th August 2017";
-$AutotestAugustCOC = "C.O.C: Michael Walsh/Michael Lynch 0862363901/0868909183";
-$CarberyNightNavigationTrialDetails = "Carbery Night Navigation Trial 25th - 26th November 2017";
-$CarberyNightNavigationTrialCOC = "C.O.C: Brian O'Mahony 0864031079";
+$LooseSurfaceAutocrossFebruaryDetails = "Loose Surface Autocross February 2019";
+$LooseSurfaceAutocrossFebruaryCOC = "C.O.C: Eric Calnan 087 7693052";
+$AutotestMayDetails = //"Autotest 20th & 21st May 2017";
+$AutotestMayCOC = //"C.O.C: Alan Clarke/DonGiles 0851744918/0868060604";
+$LooseSurfaceAutocrossJulyDetails = //"Loose Surface Autocross 8th July 2018";
+$LooseSurfaceAutocrossJulyCOC = //"C.O.C: Leslie Wolfe 0860666688";
+$AutotestAugustDetails = //"Autotest 19th & 20th August 2017";
+$AutotestAugustCOC = //"C.O.C: Michael Walsh/Michael Lynch 0862363901/0868909183";
+$CarberyNightNavigationTrialDetails = "Carbery Night Navigation Trial 24th - 25th November 2018";
+$CarberyNightNavigationTrialCOC = "C.O.C: Brian O'Mahony 086 4031079";
 
 //Email
 $emailRecipients = array('alanmugz@gmail.com, webmaster@skibbdcc.com, pro@skibbdcc.com, colmfeen@gmail.com');
